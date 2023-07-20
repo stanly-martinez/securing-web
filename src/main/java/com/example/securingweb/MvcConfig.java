d4864fe32090b305a9f090c82b5fdb3080c64c65
stanly-martinez <stanlymt3zx@gmail.com>
2023-07-19 23:01:33 -0600

    upload project

---
+++ src/main/java/com/example/securingweb/MvcConfig.java
@@ -0,0 +1,16 @@
+package com.example.securingweb;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void AddViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("Home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
